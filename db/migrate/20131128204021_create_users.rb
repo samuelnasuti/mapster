d40750d2b82c2c6582c4e7422017c13819a4f78a
Samuel Nasuti <samuel.nasuti@gmail.com>
2014-01-06 11:29:18 -0500

    asfasdf

--- db/migrate/20131128204021_create_users.rb
+++ db/migrate/20131128204021_create_users.rb
@@ -3,12 +3,8 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.float :latitude
       t.float :longitude
-      t.string :address
-      t.text :descrption
-      t.string :title
-      t.string :sales
 
-      t.timestamps
+     t.timestamps
     end
   end
 end
